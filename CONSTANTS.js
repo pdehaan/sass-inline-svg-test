4dcef31951f0f5808708d2a098ce12c74d0e2b84
Peter deHaan <peter@deseloper.com>
2016-07-22 17:31:59 -0700

    Yet more linting
    * Dedupe and lint code
    * Refactor folder names

--- CONSTANTS.js
+++ CONSTANTS.js
@@ -2,6 +2,6 @@ const { join } = require('path');
 
 module.exports = {
   IN_FILE: join(__dirname, 'static', 'main.scss'),
-  OUT_FILE: join(__dirname, 'dist', 'main.css'),
+  OUT_FILE: join(__dirname, 'dist_api', 'main.css'),
   SVG_PATH: join(__dirname, 'static', 'images')
 };
